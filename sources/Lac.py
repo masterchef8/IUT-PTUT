793f1868460bf9239b0c27f1d4c54744b45c3846
Phylante <quentin.vaudaine@gmail.com>
2015-04-10 22:03:44 +0200

    ajout lib xlrd + desambiguation Lac.py

--- sources/Lac.py
+++ sources/Lac.py
@@ -4,6 +4,7 @@ Class Lac.py
 
 Created by Somebody
 Friday 3 April 2015
+08/04/215 : renommage des variables selon la convention + desambiguation des noms de variables.
 
 """
 
@@ -13,29 +14,39 @@ class Lac:
     """
     Classe d'import et de stockage des données emmanant du .csv
     """
-    compteur = 0 # Nombre d'instance de Lac.
+    compteur = 0 # Nombre d'instances de Lac (membre static en Java).
 
-    def __init__(self, nom, code, lat, long, m2, chloMedian, chloMedSpring, chloFirst, chloSecond, chloThird, pxX, pxY, numLac,):
+    def __init__(self, nom, code, latitude, longitude, surface, chloro_median, chloro_med_spring, chloro_first, chloro_second, chloro_third, px_X, px_Y, num_lac):
         """
         :type self: object
         :param nom: Nom du lac
         :param code: Code 4 lettres du lac
-        :param lat: latitude du pixel du lac
-        :param long: longitude du pixel du lac
-        :param m2: surface du lac
-        :param chloMedian: taux chlorophyll.a median
-        :param chloMedSpring: taux chlorophyll.a median au printemps
-        :param chloFirst: taux de chlorophyll.a à la première date
-        :param chloSecond: taux de chlorophyll.a à la deuxième date
-        :param chloThird: taux de chlorophyll.a à la troisième date
-        :param pxX: pixel
-        :param numLac: Cette variable est destinée à eviter de définir des objets lac qui n'ont pas à exister
+        :param latitude: latitude du pixel du lac
+        :param longitude: longitude du pixel du lac
+        :param surface: surface du lac
+        :param chloro_median: taux chlorophyll.a median
+        :param chloro_med_spring: taux chlorophyll.a median au rpintemps
+        :param chloro_first: taux de chlorophyll.a à la première date
+        :param chloro_second: taux de chlorophyll.a à la deuxième date
+        :param chloro_third: taux de chlorophyll.a à la troisième date
+        :param px_X: pixel x
+        :param px_Y: pixel y
+        :param num_lac: Cette variable est destinée à eviter de définir des objets lac qui n'ont pas à exister
         """
-        self.nom = nom, self.code = code, self.lat = lat, self.long = long, self.m2 = m2
-        self.chloMedian = chloMedian, self.chloMedSpring = chloMedSpring, self.chloFirst = chloFirst
-        self.chloSecond = chloSecond, self.chloThird = chloThird, self.pxX = pxX
-        self.pxY = pxY, self.numLac = numLac
+        self.nom = nom
+        self.code = code
+        self.latitude = latitude
+        self.longitude = longitude
+        self.surface = surface
+        self.chloro_median = chloro_median
+        self.chloro_med_spring = chloro_med_spring
+        self.chloro_first = chloro_first
+        self.chloro_second = chloro_second
+        self.chloro_third = chloro_third
+        self.px_X = px_X
+        self.px_Y = px_Y
+        self.num_lac = num_lac
 
         Lac.compteur += 1
         if Lac.compteur > numLac:
-            raise wrongNumberExeptions
+            raise WrongNumberExeption
