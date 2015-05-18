4e67105a8728a60b715c47e4a8b57f8a94b60f9f
Philippe Giraudeau <giraudeau.philip@gmail.com>
2015-05-18 08:39:51 +0200

    Update sources/main.py

--- sources/main.py
+++ sources/main.py
@@ -10,22 +10,12 @@ import LectureCSV as lec
 import coord_manipulation
 import numpy as np
 
-"""
+
 fichier = "lac.csv"
 text = "lecture"
 lacs = []
 lecture = lec.LectureCSV(fichier)
 
 lecture.creationClassLacs()
-
-
-"""
-data = np.array([0,1,1,1,0,0,1,0,
-                 0,1,1,1,0,0,1,0,
-                 0,1,1,1,0,0,1,0])
-k = []
-
-a = data.size
-print type(a)
 
 
