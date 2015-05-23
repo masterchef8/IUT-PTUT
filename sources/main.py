6e9e2378e1ca2bcb9e457317ae772cca718fd8b3
Phylante <quentin.vaudaine@gmail.com>
2015-05-23 15:30:28 +0200

    Main fonctionne jusqu'à avoir la liste de lacs

--- sources/main.py
+++ sources/main.py
@@ -3,19 +3,30 @@
 hierarchie doit être :
 Ptut
 |-projetia
-| |-sources
-|   |-main_script_test.py
-|-Images_Calees_CONFIDENTIELLES/
+  |-sources
+  |   |-main_script_test.py
+  |-Images_Calees_CONFIDENTIELLES/
   |-Dombes-Carto-CONFIDENTIELLE.xls
 
 """
 
 from lectureXLS import LectureXLS
 from lac import Lac
+from algo_matrix import *
+from coord_manipulation import *
 
-fichier_xls = '../../Images_Calees_CONFIDENTIELLES/Dombes-Carto-CONFIDENTIELLE.xls'
-
+fichier_xls = '../Images_Calees_CONFIDENTIELLES/Dombes-Carto-CONFIDENTIELLE.xls'
+liste_images = ['../Images_Calees_CONFIDENTIELLES/20070406/06042007caletif.tif',
+                '../Images_Calees_CONFIDENTIELLES/20070805/050807caletif.tif',
+                '../Images_Calees_CONFIDENTIELLES/20071001/tif10102007calee.tif'
+                ]
 liste_lacs = LectureXLS(fichier_xls).importation() # Maintenant, on a une belle liste de lacs avec leurs infos provenant du XLS.
 
-for lac in liste_lacs:
-    print lac
+for image in liste_images:
+    for lac in liste_lacs:
+        try:
+            x, y = coord_to_px(lac.longitude, lac.latitude, image)
+        except RuntimeError:
+            pass
+        finally:
+            print "hello"
