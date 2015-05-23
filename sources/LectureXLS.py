1661d5e4cdad7f2b3e2a4e297f5875ffa47d6c16
Phylante <quentin.vaudaine@gmail.com>
2015-05-23 13:19:32 +0200

    suppr creationClassLac() doublon

--- sources/LectureXLS.py
+++ sources/LectureXLS.py
@@ -67,17 +67,3 @@ class LectureXLS:
                 ))
             return self.lacs
 
-    def creationClassLacs(self):
-        """
-        Fonction qui créé une un tableau d'objet de type Lac.
-        """
-        tabLacs = []
-        lacs = self.importation()
-        for i, elt in enumerate(lacs):
-            print lacs[i][0]
-            tabLacs[i] = Lac.Lac(lacs[i][0], lacs[i][1], lacs[i][3],
-                                 lacs[i][4], lacs[i][5], lacs[i][6],
-                                 lacs[i][7], lacs[i][8], lacs[i][9],
-                                 lacs[i][10], self.fichier_Un, self.fichier_Deux,
-                                 self.fichier_Trois)
-
