08a985768fc896b606758a747ff87b952cd5438e
Gabriel Augendre <gabriel@augendre.info>
2015-05-24 00:50:16 +0200

    Fix + increase perf of RG algorithm + fix average.

--- sources/algo_matrix.py
+++ sources/algo_matrix.py
@@ -6,7 +6,7 @@ probably only be treated as pixels arrays, so it will be easy to change.
 """
 __author__ = 'Gabriel Augendre'
 from osgeo import gdal
-import numpy
+import Queue
 
 def region_growing(matrix, beginning, threshold):
     """Region growing algorithm adapted to our problem.
@@ -23,19 +23,51 @@ def region_growing(matrix, beginning, threshold):
     if matrix[beginning.vert][beginning.hor] >= threshold:
         return None
 
-    neighbouring_pixels = [beginning]
-    neighbouring_pixels.extend(
-        beginning.compute_neighbours(len(matrix), len(matrix[0])))
-    returned_set = set()
-    returned_set.add(beginning)
-    beginning.region = True
+    # neighbouring_pixels = [beginning]
+    # neighbouring_pixels.extend(
+    #     beginning.compute_neighbours(len(matrix), len(matrix[0])))
+    # returned_set = set()
+    # returned_set.add(beginning)
+    # beginning.region = True
+    #
+    # i = 0
+    # printed = False
+    # for pixel in neighbouring_pixels:
+    #     if matrix[pixel.vert][pixel.hor] < threshold and not pixel.region:
+    #         returned_set.add(pixel)
+    #         pixel.region = True
+    #         neighbours = pixel.compute_neighbours(len(matrix), len(matrix[0]))
+    #         for neighbour in neighbours:
+    #             if i % 100 == 0 and not printed:
+    #                 print "appending pixel to neighbouring_pixels : {}".format(i)
+    #                 printed = True
+    #             if not (neighbour in neighbouring_pixels):
+    #                 neighbouring_pixels.append(neighbour)
+    #                 i += 1
+    #                 printed = False
 
-    for pixel in neighbouring_pixels:
-        if matrix[pixel.vert][pixel.hor] < threshold and not pixel.region:
-            returned_set.add(pixel)
-            pixel.region = True
-            set(neighbouring_pixels).update(
-                pixel.compute_neighbours(len(matrix), len(matrix[0])))
+    returned_set = set()
+    point_queue = Queue.Queue()
+
+    point_queue.put(beginning)
+    beginning.region = True
+    returned_set.add(beginning)
+
+    i = 0
+    printed = False
+    while not point_queue.empty():
+        if not printed and i % 1000 == 0:
+            print i
+            printed = True
+        pixel = point_queue.get()
+        neighbours = pixel.compute_neighbours(len(matrix), len(matrix[0]))
+        for neighbour in neighbours:
+            if not (neighbour in returned_set) and matrix[neighbour.vert][neighbour.hor] < threshold:
+                point_queue.put(neighbour)
+                returned_set.add(neighbour)
+                neighbour.region = True
+                i += 1
+                printed = False
 
     return returned_set
 
@@ -62,23 +94,19 @@ def lakeAverageColor(path, beginning, threshold):
     sumB = 0
     sumA = 0
 
-    print Pixel(beginning.vert, beginning.hor, RBand[beginning.vert][beginning.hor],
-                GBand[beginning.vert][beginning.hor], BBand[beginning.vert][beginning.hor],
-                ABand[beginning.vert][beginning.hor])
-    print "Taille de la region : ", len(region)
-    print "Region :"
-    print region
-
     for pixel in region:
         sumR += RBand[pixel.vert][pixel.hor]
         sumG += GBand[pixel.vert][pixel.hor]
         sumB += BBand[pixel.vert][pixel.hor]
         sumA += ABand[pixel.vert][pixel.hor]
         cptPix += 1
-    avgR = sumR / cptPix
-    avgG = sumG / cptPix
-    avgB = sumB / cptPix
-    avgA = sumA / cptPix
+
+    # Par défaut on réalise une division entière => Troncature à l'entier inférieur.
+    # On préfère réaliser une division décimale et arrondir à l'entier le plus proche.
+    avgR = int(round(float(sumR) / cptPix))
+    avgG = int(round(float(sumG) / cptPix))
+    avgB = int(round(float(sumB) / cptPix))
+    avgA = int(round(float(sumA) / cptPix))
 
     return Pixel(beginning.vert, beginning.hor, avgR, avgG, avgB, avgA)
 
@@ -106,20 +134,22 @@ class Pixel:
         """
         neighbours = set()
         if self.hor - 1 >= 0:
+            neighbours.add(Pixel(self.vert, self.hor - 1))
             if self.vert - 1 >= 0:
                 neighbours.add(Pixel(self.vert - 1, self.hor - 1))
             if self.vert + 1 < height:
                 neighbours.add(Pixel(self.vert + 1, self.hor - 1))
-            neighbours.add(Pixel(self.vert, self.hor - 1))
         if self.hor + 1 < width:
+            neighbours.add(Pixel(self.vert, self.hor + 1))
             if self.vert - 1 >= 0:
                 neighbours.add(Pixel(self.vert - 1, self.hor + 1))
             if self.vert + 1 < height:
                 neighbours.add(Pixel(self.vert + 1, self.hor + 1))
-            neighbours.add(Pixel(self.vert, self.hor + 1))
 
         if self.vert - 1 >= 0:
             neighbours.add(Pixel(self.vert - 1, self.hor))
+
+        if self.vert + 1 < height:
             neighbours.add(Pixel(self.vert + 1, self.hor))
 
         return neighbours
