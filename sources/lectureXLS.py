9a9ce73adf5719e669ad6eb04f7cdd39e54acd33
Gabriel Augendre <gabriel@augendre.info>
2015-05-23 20:31:42 +0200

    Reformat file.

--- sources/lectureXLS.py
+++ sources/lectureXLS.py
@@ -33,30 +33,29 @@ class LectureXLS:
         self.lacs = []
 
     def importation(self):
-        #Faudrait gerer les erreurs mosieur !
+        # Faudrait gerer les erreurs mosieur !
 
         with xlrd.open_workbook(self.fichier) as classeur:
-            page = classeur.sheet_by_index(0) #Feuille 1 du classeur
+            page = classeur.sheet_by_index(0)  # Feuille 1 du classeur
             for ligne in range(page.nrows):
-                if(ligne == 0):
-                    pass  #la première ligne est celle avec les en-têtes de colonnes
+                if ligne == 0:
+                    continue  # la première ligne est celle avec les en-têtes de colonnes
 
-                #Ce gros bloc est à clean il faut juste trouver le
-                #moyen de faire une boucle, une list comprehension
-                #ou autre, sachant qu'on utilise pas la colonne 3
-                #à voir, à refléchir. De plus les valeurs sont
-                #hardcoded ... bof hein :)
+                # Ce gros bloc est à clean il faut juste trouver le
+                # moyen de faire une boucle, une list comprehension
+                # ou autre, sachant qu'on utilise pas la colonne 3
+                # à voir, à refléchir. De plus les valeurs sont
+                # hardcoded ... bof hein :)
                 self.lacs.append(lac.Lac(
-                    page.cell_value(ligne, 0), #nom
-                    page.cell_value(ligne, 1), #code
-                    page.cell_value(ligne, 3), #latitude
-                    page.cell_value(ligne, 4), #longitude
-                    page.cell_value(ligne, 5), #surface
-                    page.cell_value(ligne, 6), #chloro_median
-                    page.cell_value(ligne, 7), #chloro_median_spring
-                    page.cell_value(ligne, 8), #chloro_first
-                    page.cell_value(ligne, 9), #chloro_second
-                    page.cell_value(ligne, 10) #chloro_third
+                    page.cell_value(ligne, 0),  # nom
+                    page.cell_value(ligne, 1),  # code
+                    page.cell_value(ligne, 3),  # latitude
+                    page.cell_value(ligne, 4),  # longitude
+                    page.cell_value(ligne, 5),  # surface
+                    page.cell_value(ligne, 6),  # chloro_median
+                    page.cell_value(ligne, 7),  # chloro_median_spring
+                    page.cell_value(ligne, 8),  # chloro_first
+                    page.cell_value(ligne, 9),  # chloro_second
+                    page.cell_value(ligne, 10)  # chloro_third
                 ))
             return self.lacs
-
