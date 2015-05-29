547291de8872ddfef318d30e0bda8596a09c604f
Adrien J-M. C. Rabian <adrien@rabian.fr>
2015-05-29 12:05:59 +0200

    Connecteur d'ouverture fonctionnel.

--- ihm/fenouil.py
+++ ihm/fenouil.py
@@ -2,17 +2,33 @@
  
 from PyQt4 import QtGui, QtCore
 import sys
-import FenouilMain
- 
-class FenouilMain(QtGui.QMainWindow, FenouilMain.Ui_FenouilMain):
+
+import FenouilMainUI
+
+
+class FenouilMainWindow(QtGui.QMainWindow, FenouilMainUI.Ui_FenouilMain):
     def __init__(self, parent=None):
-        super(FenouilMain, self).__init__(parent)
+        super(FenouilMainWindow, self).__init__(parent)
         self.setupUi(self)
-        self.center()
         self.connectActions()
+        #self.center()
 
     def connectActions(self):
         self.actionQuit.triggered.connect(QtGui.qApp.quit)
+        self.actionOpenImageFile.triggered.connect(lambda: self.openImage())
+
+    def openImage(self):
+        fileName = QtGui.QFileDialog.getOpenFileName(
+                        self,
+                        "Ouvrir un fichier d'image",
+                        QtCore.QDir.homePath(),
+                        "Fichiers d'image (*)"
+                    )
+        if fileName:
+            myPixmap = QtGui.QPixmap(fileName)
+            myScaledPixmap = myPixmap.scaled(self.imgDisplay.size(), QtCore.Qt.KeepAspectRatio)
+            self.imgDisplay.setPixmap(myScaledPixmap)
+            self.imgDisplay.setHidden(False)
  
     def main(self):
         self.show()
@@ -22,9 +38,9 @@ class FenouilMain(QtGui.QMainWindow, FenouilMain.Ui_FenouilMain):
         centerPoint = QtGui.QDesktopWidget().availableGeometry().center()
         frameGm.moveCenter(centerPoint)
         self.move(frameGm.topLeft())
- 
+
 if __name__=='__main__':
     app = QtGui.QApplication(sys.argv)
-    fenouilMain = FenouilMain()
+    fenouilMain = FenouilMainWindow()
     fenouilMain.main()
     app.exec_()
